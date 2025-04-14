7592b87d047c39881b21e5044ecc18a1f961534a
Daniel Soliman <73758068+danielsoliman@users.noreply.github.com>
2025-04-14 09:42:30 -0400

    Update site configuration, add Prettier, and modify site title and description

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://danny.miami',
   integrations: [mdx(), sitemap()],
 
   vite: {
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Daniel Soliman';
+export const SITE_DESCRIPTION = 'Daniel Soliman\'s personal website. A place to share my thoughts and ideas.';
