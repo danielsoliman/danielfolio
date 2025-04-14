b9525af0e8c2e1a6956c191e614c237fc1e63e85
Daniel Soliman <73758068+danielsoliman@users.noreply.github.com>
2025-04-14 10:36:37 -0400

    updated opengraph

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Daniel Soliman';
-export const SITE_DESCRIPTION = 'Daniel Soliman\'s personal website. A place to share my thoughts and ideas.';
+export const SITE_DESCRIPTION = 'Construction Scheduler and Frontend Engineer. Get in my head.';
